23d95d52576c418975dce9b8d7018c5bea3ef330
Christophe <pro.chrisrln@gmail.com>
2021-04-20 13:42:40 +0200

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Chris on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
